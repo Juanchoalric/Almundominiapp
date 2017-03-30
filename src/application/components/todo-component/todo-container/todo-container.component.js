be3bc267488627e587b5ce27f3b801b70054ba7a
hernimen <hernanman_1@hotmail.com>
2017-03-30 11:41:50 -0300

    agrego de services

--- src/application/components/todo-component/todo-container/todo-container.component.js
+++ src/application/components/todo-component/todo-container/todo-container.component.js
@@ -15,33 +15,36 @@
         let self = this;
 
         this.$onInit = function () {
-            self.tasks = [
-                {
-                  id: 1,
-                  task: "Terminar el proyecto"
-                },
-                {
-                  id: 2,
-                  task: "Empezar el proyecto"
-                }
-            ];
+
+          self.tasks = TodoService.getTasks();
+            // self.tasks = [
+            //     {
+            //       id: 1,
+            //       task: "Terminar el proyecto"
+            //     },
+            //     {
+            //       id: 2,
+            //       task: "Empezar el proyecto"
+            //     }
+            // ];
         };
 
         this.addTask = function ($event) {
             let task = $event.task;
-
-            self.tasks.push({
-                id: self.tasks.length + 1,
-                task: task
-            });
+            self.tasks = TodoService.addTask();
+            // self.tasks.push({
+            //     id: self.tasks.length + 1,
+            //     task: task
+            // });
         };
 
-        // this.deleteTask = function ($event) {
-        //   let task = $event. task;
-        //
-        //   self.tasks = self.tasks.filter(function(){
-        //     return task.id !== item.id;
-        //   });
-        // };
+        this.deleteTask = function ($event) {
+          let task = $event. task;
+
+          self.tasks = TodoService.deleteTask();
+          // self.tasks = self.tasks.filter(function(){
+          //   return task.id !== item.id;
+          // });
+        };
     }
 })();
---
+++ src/application/components/todo-component/todo-container/todo-container.services.js
@@ -0,0 +1,38 @@
+(function (){
+  'use strict';
+
+  angular
+  .module('todoComponent')
+  .service('TodoService', TodoService);
+
+  function TodoService(){
+    this.getTasks = getTasks;
+
+    function getTasks(){
+      return [
+      {
+        id:1,
+        task:"Terminar el proyecto",
+        finished: false
+      },
+      {
+        id:2,
+        task: "Empezar el proyecto",
+        finished: true
+      }];
+    }
+
+    function addTask(){
+      this.tasks.push({
+        id: this.tasks.lenght + 1,
+        task: task
+      });
+    };
+
+    function deleteTask(){
+      this.tasks = this.tasks.filter(function(){
+          return task.id !== item.id;
+        });
+    }
+  }
+})();
